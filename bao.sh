5e5c9a558f972fd50fd1474253cf5001a7b31ae4
Martin Tech <75930741+MartinTech63@users.noreply.github.com>
2024-06-23 00:14:42 +0200

    Update bao.sh

--- bao.sh
+++ bao.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Ajouter les alias boiteaoutils et bao
+add_aliases() {
+    alias boiteaoutils='bash -c "$(wget -qLO - https://script.lttdm.ovh/)"'
+    alias bao='bash -c "$(wget -qLO - https://script.lttdm.ovh/)"'
+}
+
 # Vérifier si le script est exécuté par root
 check_root() {
     if [[ $(id -u) -ne 0 ]]; then
